619ae915604441cd50c7f47a1327dd2de4bf2bb0
kyuma-git <hokumon2@icloud.com>
2020-07-23 09:54:24 +0900

    Initial Commit

---
+++ github_issues_list_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github_issues_list_app
+//
+//  Created by kyuma.morita on 2020/07/23.
+//  Copyright © 2020 kyuma.morita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
