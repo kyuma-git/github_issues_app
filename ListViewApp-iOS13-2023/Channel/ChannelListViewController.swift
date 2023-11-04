c97e5868bcdf77506850375294f824034a9a6e76
Kyuma Morita <kyuma.morita@MLEA2203.local>
2023-11-04 14:51:37 +0900

    Create base files

---
+++ ListViewApp-iOS13-2023/Channel/ChannelListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChannelListViewController.swift
+//  ListViewApp-iOS13-2023
+//
+//  Created by Kyuma Morita on 2023/11/04.
+//  Copyright © 2023 kyuma.morita. All rights reserved.
+//
+
+import UIKit
+
+final class ChannelListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
